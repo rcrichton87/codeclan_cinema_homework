3931a012ae95eb654916f5efd410ecdee7f5f1b7
Ross Crichton <rcrichton87@googlemail.com>
2017-02-17 11:47:34 +0000

    added save method to ticket class

--- models/customer.rb
+++ models/customer.rb
@@ -4,24 +4,24 @@ require_relative('./ticket.rb')
 
 class Customer
 
-attr_reader :id
-attr_accessor :name, :funds
+  attr_reader :id
+  attr_accessor :name, :funds
 
-def initialize(options)
-  @id = options['id'].to_i
-  @name = options['name']
-  @funds = options['funds'].to_i
-end
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
 
-def save
-  sql = "INSERT INTO customers (name, funds) VALUES ('#{@name}', #{@funds}) RETURNING *;"
-  customer = SqlRunner.run(sql).first
-  @id = customer['id'].to_i
-end
+  def save
+    sql = "INSERT INTO customers (name, funds) VALUES ('#{@name}', #{@funds}) RETURNING *;"
+    customer = SqlRunner.run(sql).first
+    @id = customer['id'].to_i
+  end
 
-def self.delete_all
-  sql = "DELETE FROM customers;"
-  SqlRunner.run(sql)
-end
+  def self.delete_all
+    sql = "DELETE FROM customers;"
+    SqlRunner.run(sql)
+  end
 
 end
--- models/ticket.rb
+++ models/ticket.rb
@@ -4,14 +4,20 @@ require_relative('../db/sql_runner.rb')
 
 class Ticket
 
-attr_reader :id, :customer_id, :film_id
-attr_accessor :time
+  attr_reader :id, :customer_id, :film_id
+  attr_accessor :time
 
-def initialize(options)
-  @id = options['id'].to_i
-  @customer_id = options['customer_id'].to_i
-  @film_id = options['film_id'].to_i
-  @time = options['time']
-end
+  def initialize(options)
+    @id = options['id'].to_i
+    @customer_id = options['customer_id'].to_i
+    @film_id = options['film_id'].to_i
+    @time = options['time']
+  end
+
+  def save
+    sql = "INSERT INTO ticket (customer_id, film_id, time) VALUES (#{@customer_id}, #{@film_id}, '#{@time}') RETURNING *;"
+    ticket = SqlRunner.run(sql).first
+    @id = ticket['id'].to_i
+  end 
 
 end
